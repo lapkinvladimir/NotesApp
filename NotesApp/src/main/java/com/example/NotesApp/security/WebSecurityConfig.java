f9a65a626affbec7afbb35b431e2e37a627d5d63
lapkinvladimir <lapkinvladimir12@gmail.com>
2023-08-11 02:28:00 +0200

    The WebSecurityConfig is configured to allow access to h2-console

--- NotesApp/src/main/java/com/example/NotesApp/security/WebSecurityConfig.java
+++ NotesApp/src/main/java/com/example/NotesApp/security/WebSecurityConfig.java
@@ -8,9 +8,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class WebSecurityConfig{
+
+
+
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         UserDetails user = User.withDefaultPasswordEncoder()
@@ -30,8 +34,19 @@ public class WebSecurityConfig{
 
     }
 
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().requestMatchers("/**").hasRole("ADMIN").and().formLogin();
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
+                )
+                .headers(headers -> headers.frameOptions().disable())
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")));
         return http.build();
     }
+
+
+
+
 }
